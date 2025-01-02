cad2c3ffb795afdf23a8d28bec0b50613ff7c797
Prethi12 <152271388+Prethi12@users.noreply.github.com>
2025-01-02 11:12:05 +0530

    Update App.jsx

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { Login, SignIn } from "./pages"; 
+import Report from "./pages/Report";
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<SignIn />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Report />} />
       </Routes>
     </Router>
   );
